5ab151ebd8136b9946811ad861fb143bafff8b32
Master60 <habebelrhman321@gmail.com>
2024-04-11 20:12:30 +0200

    Top Communities - Creating the layout

---
+++ src/Components/topcommunities/TopCommunities.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../navbar/Navbar";
+import CommunitiesSection from "./CommunitiesSection";
+
+const TopCommunities = () => {
+  return (
+    <>
+      <div className="bg-[#0b1416]">
+        <Navbar />
+        <CommunitiesSection />
+      </div>
+    </>
+  );
+};
+
+export default TopCommunities;
