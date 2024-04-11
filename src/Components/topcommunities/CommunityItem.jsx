a2b677a68907a6622d819534e735ae526dc75ef7
Master60 <habebelrhman321@gmail.com>
2024-04-12 01:32:51 +0200

    Updating displays of community name & members

--- src/Components/topcommunities/CommunityItem.jsx
+++ src/Components/topcommunities/CommunityItem.jsx
@@ -14,7 +14,7 @@ const CommunityItem = ({ index, name, icon, topic, members }) => {
     <>
       <div className="flex flex-wrap justify-center py-[0.75rem] h-[76px] xs:border-t xs:border-gray-700 sm:border-none">
         <h6 className="flex flex-col font-bold justify-center items-center text-[12px] w-[48px] m-0 truncate text-[#FFFFFF]">
-          {index}
+          {index + 1}
         </h6>
         <span className="flex flex-col items-center justify-center px-[8px] w-[52px]">
           <div className="rounded-full m-0 select-none w-[36px] h-[36px]">
@@ -31,13 +31,13 @@ const CommunityItem = ({ index, name, icon, topic, members }) => {
         </span>
         <div className="flex flex-col flex-grow items-start">
           <a className="m-0 font-bold text-[12px] truncate max-w-[11rem] text-[#F2F2F2]">
-            {name}
+            r/{name}
           </a>
           <h6 className="flex-grow h-[20px] text-[12px] truncate py-[0.125rem] w-[11rem] m-0 text-[#F2F2F2]">
             {topic}
           </h6>
           <h6 className="text-[12px] m-0 truncate w-[11rem] text-[#82959B]">
-            {members}
+            {members} members
           </h6>
         </div>
       </div>
