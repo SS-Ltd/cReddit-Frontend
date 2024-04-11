b99987257e74656407224db81fc1de008668c1c7
Master60 <habebelrhman321@gmail.com>
2024-04-11 20:14:56 +0200

    Communities Section - Header

---
+++ src/Components/topcommunities/CommunitiesSection.jsx
@@ -0,0 +1,21 @@
+const CommunitiesSection = () => {
+  return (
+    <>
+      <section className="box-border p-[1rem] relative max-w-[1200px] mx-auto mb-[1rem] block break-words leading-[1.5rem]">
+        <header className="p-0 my-[1rem] mx-[0rem]">
+          <h1 className="flex items-center justify-center font-bold mt-[64px] mb-[0.25rem] mx-0 text-[1rem] leading-[1.25rem] ms-0 me-0 text-[#F2F2F2]">
+            Best of Reddit
+          </h1>
+          <h2 className="font-bold mt-[14px] mb-[0.25rem] mx-0 text-[0.875rem] text-[#F2F2F2] ms-0 me-0">
+            Top Communities
+          </h2>
+          <h2 className="font-normal mt-[0.25rem] mx-0 text-[0.75rem] leading-[1rem] text-[#82959B] block ms-0 me-0">
+            Browse Reddit's largest communities
+          </h2>
+        </header>
+      </section>
+    </>
+  );
+};
+
+export default CommunitiesSection;
